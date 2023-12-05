82ba047355a7c4fcb6d0f731d736641cec779b75
SecretSatyr <razvandrosu1@gmail.com>
2023-12-05 09:32:13 +0000

    Prima Versione

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Cifriamo il nostro testo e trasformiamolo in un testo segreto!");
+    Matrice m = new Matrice("TPSIT");
+    Vigenere v = new Vigenere(0,12,0,12,m);
+    thread t = new thread(v);
+    t.start(); 
+    //commento
+  }
+}
